e68b4b8ebaa4c1594440d823394149647fbb2d80
Allison <allisonarchi@gmail.com>
2025-05-26 19:45:42 -0400

    Retos_sesion04 ejercicio01

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,11 @@
+# ejercicio_01.py
+# Ejercicio: Crear un triángulo rectángulo utilizando la función print.
+# Este programa mostrará un triángulo rectángulo simple.
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
