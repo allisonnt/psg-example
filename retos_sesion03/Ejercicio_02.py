249707d7173a3d3f6f965bf6e147a49456f18ea9
Allison <allisonarchi@gmail.com>
2025-05-26 14:59:45 -0400

    adiccion del ejercicio02_reto3

---
+++ retos_sesion03/Ejercicio_02.py
@@ -0,0 +1,8 @@
+print("YO: ¡Hola, GATITA! ¿Cómo estás hoy?")
+print("Gato: Miau... no me puedo quejar.")
+print("YO: ¿hace mucho frio, quierees que te ponga tu chompa?")
+print("Gato: Miau miau... no te atreverias")
+print("YO: Está bien, te daré tu comida favorita.")
+print("Gato: ¡Miau! croquetas de pollo ya sabes.")
+print("YO: ¡lo que tu digas!Bueno, voy a trabajar en eso")
+print("Gato: Miau... adiós espero mi comida Miauuuuu.")
