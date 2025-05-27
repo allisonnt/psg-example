51d392a29ccdd527cb5370158a35ac4c30fc5bec
Allison <allisonarchi@gmail.com>
2025-05-27 01:04:11 -0400

    ejercicio_04.py - reto5

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,9 @@
+# EJERCICIO 4
+# Medida del lado del cubo en metros
+lado = 15
+
+# Cálculo del volumen
+volumen = lado ** 3
+
+# Mostrar el resultado
+print("El volumen del cubo es:", volumen, "metros cúbicos")
