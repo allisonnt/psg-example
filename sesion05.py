60051084ec6484d258313c95297a7f8db91667e8
Allison <allisonarchi@gmail.com>
2025-05-27 00:31:19 -0400

    sesion6.py

---
+++ sesion05.py
@@ -0,0 +1,118 @@
+import operator
+
+
+print (type (1))
+
+x1q3z9ahd = 35.0
+x1q3z9afd = 12.50
+x1q3p9afd = x1q3z9ahd * x1q3z9afd
+print (x1q3p9afd)
+
+print (10)
+print (type(10))
+
+variable = 100
+print (variable)
+print (type(variable))
+
+variable_2 = int(20.0)
+print (variable_2)
+print (type(variable_2))
+
+print("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("base binaria")
+print (0b1010)
+# Valor 10 en octal 
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal 
+print ("Base hexadecimal")
+print (0xa)
+
+variable_3 = 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+print (variable_3)
+print (type(variable_3))
+
+# Valor 0.5 Flotante
+print (0.5)
+print (type(0.5))
+
+variable_4 = 0.100546
+print(variable_4)
+print (type (variable_4))
+
+variable_7 = float (1)
+print(variable_7)
+print (type (variable_7))
+
+variable_6 = 2.0e-3
+print(variable_6)
+print (type (variable_6))
+
+# Precision de 17 decimales
+variable_5 = 0.999999999999999
+print(variable_5)
+print (type (variable_5))
+
+# Operadores aritmeticos
+a = 10
+b = 3
+# Suma
+print ("suma")
+print (a + b)
+# Resta
+print ("resta")
+print (a - b)
+# Multiplicacion 
+print ("multiplicacion")
+print (a * b)
+# Division
+print ("division")
+print (a / b)
+# Potencia
+print ("Potencia")
+print (a ** b)
+# Modulo o residuo
+print ("modulo o residuo")
+print (a % b)
+# Division entera
+print ("Division entera")
+print (a // b)
+
+# EJERCICIO 1
+# Minutos y segundos dados
+minutos = 300
+segundos = 3600
+
+# Convertir minutos a segundos
+total_segundos = minutos * 60 + segundos
+
+# Convertir segundos totales a horas
+total_horas = total_segundos / 3600
+
+# Mostrar el resultado
+print("El total en horas es:", total_horas)
+
+print ("operator de comparacion")
+comparar = 10
+print (comparar > 10)
+print (comparar < 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+
+
+
+print ("operadores de comparacion con int - float")
+entero = 12
+flotante = 12.1
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
+
