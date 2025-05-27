4c5052ce509e4c9bab33627c2d6b18b77ef2c9ed
Allison <allisonarchi@gmail.com>
2025-05-27 00:48:16 -0400

    ejercicio_01 - reto5

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,12 @@
+# Practica el uso de numeros enteros y con punto flotante
+# Precio por producto
+precio = 3.75
+
+# Cantidad de productos
+cantidad = 12
+
+# Cálculo del total
+total = precio * cantidad
+
+# Mostrar resultado
+print("Total a pagar: $", total)
