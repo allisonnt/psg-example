1569492ef52f8013e6209f98e21988c04dd51bd3
Allison <allisonarchi@gmail.com>
2025-05-28 00:40:19 -0400

    paridad

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,11 @@
+# Comparar 44 y 98
+n1 = 44
+n2 = 98
+misma_paridad1 = (n1 % 2) == (n2 % 2)
+print(misma_paridad1)
+
+# Comparar 111 y 333
+n3 = 111
+n4 = 333
+misma_paridad2 = (n3 % 2) == (n4 % 2)
+print(misma_paridad2)
