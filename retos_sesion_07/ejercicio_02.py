7e940a6410b43f89cbf939487223ed9da0357790
Allison <allisonarchi@gmail.com>
2025-06-01 15:43:28 -0400

    ejercicio_02.py resta

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,5 @@
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+c = int(input("Ingresa el tercer número: "))
+resultado = a - b - c
+print("El resultado de la resta es:", resultado)
