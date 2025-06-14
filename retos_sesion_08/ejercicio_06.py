d474e299472fcb3c3b743ab8cbe6c0f78a090bd7
Allison <allisonarchi@gmail.com>
2025-06-14 10:44:27 -0400

    ejercicio6

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,6 @@
+punto1 = (-20, -20)
+punto2 = (40, 20)
+x_medio = (punto1[0] + punto2[0]) / 2
+y_medio = (punto1[1] + punto2[1]) / 2
+punto_medio = (x_medio, y_medio)
+print("El punto medio es:", punto_medio)
