4899382e309d28a7b0dc9c955b7918bccf933d7d
Allison <allisonarchi@gmail.com>
2025-06-14 10:39:25 -0400

    ejercicio5

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,8 @@
+guia_telefonica = (
+    ("Jhon", 591123456, "Villa Fátima"),
+    ("Jane", 591546324, "Obrajes"),
+    ("Dogg", 591222421, "Miraflores"),
+    ("Kity", 591753159, "Achumani")
+)
+print("Guía telefónica:")
+print(guia_telefonica)
