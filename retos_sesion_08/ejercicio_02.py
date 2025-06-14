d32d5839228a92f7887b3afc52528b0e48781c01
Allison <allisonarchi@gmail.com>
2025-06-14 10:23:33 -0400

    ejercicio02

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,6 @@
+letras = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+print("Primer elemento:", letras[0])
+print("Último elemento:", letras[-1])
+print("Slice del índice 3 al 5:", letras[3:6])
+print("Slice del índice 5 al 9 con pasos de 3:", letras[5:10:3])
+print("Slice del índice 9 al 0 con pasos de -2:", letras[9::-2])
