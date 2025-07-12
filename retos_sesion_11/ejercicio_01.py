956931324b73774bc606da0505d04c7474e56f0b
Allison <allisonarchi@gmail.com>
2025-07-11 22:42:52 -0400

    ejercicios 01 sesion 11

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,10 @@
+animal_marino = {
+    'especie': 'delfín',
+    'habitat': 'océano Pacífico',
+    'dieta': 'pescado y calamares',
+    'estado_salud': 'saludable',
+    'edad': 8,
+    'responsables': {'Carlos', 'Lucía', 'Martín'}
+}
+print(animal_marino)
+print(type(animal_marino))
