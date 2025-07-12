69caab668098905516b925f3f908418d5dfc373c
Allison <allisonarchi@gmail.com>
2025-07-12 17:46:18 -0400

    ejercicio 02 sesion 12

---
+++ retos_sesion_12/ejercicio_02.py
@@ -0,0 +1,10 @@
+print("Inicio verificación de acceso")
+
+usuario_logueado = True  # Cambia a False para probar el mensaje de error
+
+if usuario_logueado:
+    print("Acceso concedido a la página web")
+else:
+    print("Error: Usuario no ha iniciado sesión")
+
+print("Fin")
