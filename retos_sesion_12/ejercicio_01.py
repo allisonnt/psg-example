a92298e7d514fbd6fc68b193eef01fcb51c61d0f
Allison <allisonarchi@gmail.com>
2025-07-12 17:41:43 -0400

    ejercicio01 sesion 12

---
+++ retos_sesion_12/ejercicio_01.py
@@ -0,0 +1,7 @@
+print("Inicio Ternario Par o Impar")
+
+numero = int(input("Ingrese un número entero: "))
+resultado = "El número es par" if numero % 2 == 0 else "El número es impar"
+print(resultado)
+
+print("Fin")
