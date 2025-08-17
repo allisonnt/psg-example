be30783f2f14c556f9098f7db6ddc2f23c64e961
Allison <allisonarchi@gmail.com>
2025-08-16 21:03:52 -0400

    modificacion del ejercicio 4

--- retos_sesion_13/ejercicio_04.py
+++ retos_sesion_13/ejercicio_04.py
@@ -1,13 +1,9 @@
-print("Ejercicio 4: Verificador de palíndromos")
+print("Ejercicio 4")
 while True:
     frase = input("Ingrese una frase: ")
     if "salir" in frase:
         break
-    invertida = ""
-    for letra in frase:
-        invertida = letra + invertida  # construimos la frase invertida al inicio
-    if frase == invertida:
-        print("Es un palíndromo")
-    else:
-        print("No es un palíndromo")
-print("Fin del programa")
+    # Convertir todo a minúsculas y quitar espacios para comparar
+    frase_limpia = frase.replace(" ", "").lower()
+    print("Palíndromo" if frase_limpia == frase_limpia[::-1] else "No es palíndromo")
+print("Fin")
