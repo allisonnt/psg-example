e5142abbfd742e7b98bad8cae42e24fafe0c332c
Allison <allisonarchi@gmail.com>
2025-08-16 20:20:44 -0400

    re resolucion del ejercicio 4 sesion 12

--- retos_sesion_12/ejercicio_04.py
+++ retos_sesion_12/ejercicio_04.py
@@ -1,15 +1,13 @@
-print("Inicio cálculo de descuento")
+print("Inicio Descuento Tienda")
 
-edad = int(input("Ingrese la edad del cliente: "))
-compra = float(input("Ingrese el monto de la compra: "))
+edad = int(input("Ingrese su edad: "))
+compra = float(input("Ingrese el monto de su compra: "))
 
-if edad >= 18 and compra > 1000:
-    descuento = 10
-elif edad < 18 and compra > 500:
-    descuento = 5
+if edad > 60 and compra > 1000:
+    print("Se aplica un descuento del 20%")
+elif edad >= 18 and edad <= 60 and compra > 500:
+    print("Se aplica un descuento del 10%")
 else:
-    descuento = 2
-
-print("Descuento aplicado:", descuento, "%")
+    print("Se aplica un descuento del 2%")
 
 print("Fin")
